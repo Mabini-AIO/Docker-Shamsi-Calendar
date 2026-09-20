a3e465169df89db160c8347d2dea99adb5c03ea9
Mohammad Mahdi Hajimobini <m.mahdi_hajimobini@outlook.com>
2026-09-20 23:00:07 +0330

    changed port settings

--- Docker-Shamsi-Calendar/Program.cs
+++ Docker-Shamsi-Calendar/Program.cs
@@ -4,7 +4,7 @@ using Docker_Shamsi_Calendar.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
+builder.WebHost.UseUrls("http://0.0.0.0:8574");
 
 builder.Services.AddSingleton<DatabaseService>();
 builder.Services.AddHostedService<TimeIrScraperService>();
