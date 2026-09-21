16b38082ef744b002a7ebe2041fa17683510f591
Mohammad Mahdi Hajimobini <m.mahdi_hajimobini@outlook.com>
2026-09-21 15:20:09 +0330

    added isPermanent in DB

--- Docker-Shamsi-Calendar/Models/EventModels.cs
+++ Docker-Shamsi-Calendar/Models/EventModels.cs
@@ -1,5 +1,5 @@
 ﻿namespace Docker_Shamsi_Calendar.Models
 {
-    public record AddEventRequest(string Title, int Year, int Month, int Day, bool IsHoliday = false);
-    public record CalendarEvent(int Id, string Title, int Year, int Month, int Day, DateTime GregorianDate, bool IsHoliday, bool IsCustom);
+    public record CalendarEvent(int Id, string Title, int Year, int Month, int Day, DateTime GregorianDate, bool IsHoliday, bool IsCustom, bool IsPermanent = false);
+    public record AddEventRequest(string Title, int Year, int Month, int Day, bool IsHoliday, bool IsPermanent);
 }
